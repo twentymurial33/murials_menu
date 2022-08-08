90bc5278c6cf1546af02671b5921e9edbf76ecf9
Murial Anindo <murial.anindo@lonett.com>
2022-08-08 16:46:01 -0400

    added the filtering but query is not fetching data

--- src/pages/Filtering.js
+++ src/pages/Filtering.js
@@ -1,12 +1,23 @@
 import React from "react";
 import Layout from "../components/Layout";
+import { useQuery } from "react-query";
 
 function Filtering() {
+  const { isLoading, error, data } = useQuery("menu_items", () =>
+    fetch("https://demo5940257.mockable.io/menu_items").then((response) =>
+      response.json()
+    )
+  );
+  if (isLoading) return "Loading.";
+  if (error) return "Error has occurred: " + error.message;
   return (
     <div>
-      <Layout></Layout>
+      <Layout>
+        <h1>{data.img}</h1>
+        <p>{data.title}</p>
+        <p>{data.author}</p>
+      </Layout>
     </div>
   );
 }
-
 export default Filtering;
