206b67df4313d4a45fd16d73d8b911b6a703d5f7
Murial Anindo <murial.anindo@lonett.com>
2022-08-22 14:19:00 -0400

    all working yay

--- client/src/pages/Filtering.js
+++ client/src/pages/Filtering.js
@@ -4,55 +4,51 @@ import { useState } from "react";
 import Layout from "../components/Layout";
 import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
-import TextField from "@mui/material/TextField";
 import "../index.css";
-//Search Component- Read in CRUD
-export default function App() {
-  const SearchBar = ({ setSearchQuery }) => (
-    <form>
-      <TextField
-        id="search-bar"
-        className="text"
-        onInput={(e) => {
-          setFilter(e.target.value);
-        }}
-        label="Search Food "
-        variant="outlined"
-        placeholder="Search..."
-        size="small"
-        style={{ marginTop: "20px", width: "340px", height: "60px" }}
-      />
-      <IconButton type="submit" aria-label="search">
-        <SearchIcon style={{ fill: "blue", marginTop: "20px" }} />
-      </IconButton>
-    </form>
-  );
 
-  const [filter, setFilter] = useState(" ");
-  const [search, setSearchQuery] = useState("");
-  const { isLoading, data, error } = useQuery(["posts", filter], () =>
+//Search Component- Read in CRUD
+
+function App() {
+  const [search, setNewSearch] = useState("");
+  const { isLoading, data, error } = useQuery(["posts"], () =>
     axios("https://demo5940257.mockable.io/menu_items")
   );
   if (error) return <h2>Error </h2>;
   if (isLoading) return <h2> isLoading </h2>;
-  // console.log(data);
+  console.log(data);
 
-  const filtered = data.data.filter((data) => {
-    return data.title === "Burger";
-  });
+  const handleSetSearch = (e) => {
+    setNewSearch(e.target.value);
+  };
+
+  const filtered = !search
+    ? data.data
+    : data.data.filter((data) =>
+        data.title.toLowerCase().includes(search.toLowerCase())
+      );
 
   return (
-    <div>
+    <div className="app">
       <Layout />
-      <SearchBar searchQuery={search} setSearchQuery={setFilter} />
-      {filtered.map((data) => {
-        return (
-          <div key={data.id}>
-            <h2>name: {data.title}</h2>
-            <img src={data.img} alt="" />
-          </div>
-        );
-      })}
+      <input
+        className="search"
+        placeholder="Search..."
+        value={search}
+        onChange={handleSetSearch}
+      />
+      <IconButton type="submit" aria-label="search">
+        <SearchIcon style={{ fill: "blue", marginTop: "20px" }} />
+      </IconButton>
+      <ul className="list">
+        {filtered.map((data) => {
+          return (
+            <p key={data.id}>
+              <img src={data.img}></img>
+            </p>
+          );
+        })}
+      </ul>
     </div>
   );
 }
+export default App;
