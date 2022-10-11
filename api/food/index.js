fa42af5799ba1a5ff26b52f71a500ff3431e75f7
Murial Anindo <murial.anindo@lonett.com>
2022-10-11 17:06:02 -0400

    updated api

--- api/food/index.js
+++ api/food/index.js
@@ -2,12 +2,22 @@ import prisma from "../../client/src/lib/util";
 
 export default async function handle(req, res) {
   const { img, author, title } = req.body;
-  const foodItem = await prisma.food.create({
+  const { id } = req.params;
+  const food = await prisma.food.create({
     data: {
       title: title,
       author: author,
       img: img,
     },
   });
+  const foodItem = await prisma.food.update({
+    where: { id: Number(id) },
+    data: {
+      img,
+      author,
+      title,
+    },
+  });
+  res.json(food);
   res.json(foodItem);
 }
