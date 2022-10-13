2993c1bf5d0b01eb10ef198b26dd57328d1cc933
Murial Anindo <murial.anindo@lonett.com>
2022-10-13 08:36:35 -0400

    started fecthing data and adding to the add pages

--- client/src/pages/AddFood.js
+++ client/src/pages/AddFood.js
@@ -2,21 +2,13 @@ import React from "react";
 import axios from "axios";
 import { useQuery } from "react-query";
 
-export default function AddFood({ food }) {
-  const [loading, error, data] = useQuery(food, () =>
+function AddFood() {
+  const { isLoading, data, error } = useQuery(["food"], () =>
     axios("http://localhost:3000/api/food")
   );
-  if (error) return <h2>Error </h2>;
-  if (loading) return <h2> loading </h2>;
+
   console.log(data);
-  return (
-    <div>
-      return (
-      <div>
-        <button>Add Me</button>
-        <p>{food.Name}</p>
-      </div>
-      );
-    </div>
-  );
+  return <div className="gallery"></div>;
 }
+
+export default AddFood;
--- client/src/pages/EditFood.js
+++ client/src/pages/EditFood.js
@@ -1,31 +1,21 @@
 import React from "react";
+import Layout from "../components/Layout";
 import Button from "@mui/material/Button";
 import MenuItem from "@mui/material/MenuItem";
 import EditIcon from "@mui/icons-material/Edit";
-import Divider from "@mui/material/Divider";
-import ArchiveIcon from "@mui/icons-material/Archive";
 import FileCopyIcon from "@mui/icons-material/FileCopy";
-import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 function EditFood() {
-  const [anchorEl, setAnchorEl] =
-    (React.useState < null) | (HTMLElement > null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
+  const handleClick = (event) => {};
+  const handleClose = () => {};
 
   return (
     <div>
+      <Layout />
       <Button
         id="demo-customized-button"
-        aria-controls={open ? "demo-customized-menu" : undefined}
         aria-haspopup="true"
-        aria-expanded={open ? "true" : undefined}
         variant="contained"
         disableElevation
         onClick={handleClick}
@@ -33,7 +23,6 @@ function EditFood() {
       >
         Options
       </Button>
-
       <MenuItem onClick={handleClose} disableRipple>
         <EditIcon />
         Edit
@@ -41,15 +30,6 @@ function EditFood() {
       <MenuItem onClick={handleClose} disableRipple>
         <FileCopyIcon />
         Duplicate
-      </MenuItem>
-      <Divider sx={{ my: 0.5 }} />
-      <MenuItem onClick={handleClose} disableRipple>
-        <ArchiveIcon />
-        Archive
-      </MenuItem>
-      <MenuItem onClick={handleClose} disableRipple>
-        <MoreHorizIcon />
-        More
       </MenuItem>
     </div>
   );
