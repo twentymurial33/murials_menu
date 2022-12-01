8b806a7ed43e3d86b956601d7fe83c5328fa5055
Murial Anindo <murial.anindo@lonett.com>
2022-12-01 15:07:18 -0500

    final page edit

--- client/src/pages/EditForm.js
+++ client/src/pages/EditForm.js
@@ -1,11 +1,25 @@
+import * as React from "react";
 import Button from "@mui/material/Button";
-import Form from "@mui/material/FormControl";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
 import { useState } from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
 
-const EditForm = () => {
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme();
+
+export default function EditForm() {
   const [editing, setEditing] = useState(false);
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
   };
 
   function isEditing(id) {
@@ -13,29 +27,68 @@ const EditForm = () => {
     setEditing(id);
   }
 
-  //   const editItem = (id) => {
-  //     const newEditItem = data.find(() => {
-  //       return data.id === id;
-  //     });
-  //     console.log(newEditItem);
-  //   };
+  //   //   const editItem = (id) => {
+  //   //     const newEditItem = data.find(() => {
+  //   //       return data.id === id;
+  //   //     });
+  //   //     console.log(newEditItem);
+  //   //   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form>
-        <Form
-          as="textarea"
-          placeholder="Enter Food Item"
-          rows={3}
-          name="address"
-        />
-      </Form>
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Edit
+          </Typography>
 
-      <Button variant="success" type="submit" block onClick={isEditing}>
-        Edit Food Item
-      </Button>
-    </Form>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <hr />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Food Name"
+              name="food"
+              autoComplete="food"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="Author"
+              label="Author"
+              type="author"
+              id="author"
+              autoComplete="author"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              onClick={isEditing}
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Save
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
-};
-
-export default EditForm;
+}
