62807c6da864cd3225d739359342ee061c19b6f5
Murial Anindo <murial.anindo@lonett.com>
2022-12-30 11:34:31 -0500

    adding a single route

--- server/index.js
+++ server/index.js
@@ -8,11 +8,23 @@ const prisma = new PrismaClient();
 app.use(cors());
 app.use(express.json());
 
+//endpoint to get Many
 app.get("/menu_items", async (req, res) => {
   const getFood = await prisma.food.findMany();
   res.json(getFood);
 });
 
+//Add server endpoint to GET a single food item
+app.get("/menu_items", async (req, res) => {
+  const getFoodItem = await prisma.food.findOne({
+    where: {
+      id: Number(id),
+    },
+  });
+  res.json(getFoodItem);
+});
+
+//Post menthod
 app.post("/food", async (req, res) => {
   const { img, author, title } = req.body;
   console.log(req.body);
@@ -26,6 +38,7 @@ app.post("/food", async (req, res) => {
   res.json(food);
 });
 
+//delete method
 app.delete("/food/:id", async (req, res) => {
   const { id } = req.params;
   const deletedFood = await prisma.food.delete({
@@ -36,6 +49,7 @@ app.delete("/food/:id", async (req, res) => {
   res.json(deletedFood);
 });
 
+//put method
 app.put("/food/:id", async (req, res) => {
   const { img, author, title } = req.body;
   const { id } = req.params;
