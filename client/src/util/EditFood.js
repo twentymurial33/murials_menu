ba9c03606b23625ebec1b736cd9a50937b1fcb8d
Murial Anindo <murial.anindo@lonett.com>
2023-01-11 12:48:26 -0500

    added the utility function

--- client/src/util/EditFood.js
+++ client/src/util/EditFood.js
@@ -1,6 +1,8 @@
-const result = {
-  data: null,
-  error: null,
-};
+export const editFood = () => {
+  const result = {
+    data: null,
+    error: null,
+  };
 
-console.log(result.data);
+  console.log(result.data);
+};
