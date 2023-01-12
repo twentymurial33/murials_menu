86d13b43a6e7b8d2c9080edfac4dd3c73f88d722
Murial Anindo <murial.anindo@lonett.com>
2023-01-12 10:13:56 -0500

    onsuccess and onerror callbacks

--- client/src/util/EditFood.js
+++ client/src/util/EditFood.js
@@ -1,7 +1,16 @@
-export const editFood = () => {
+export const editFood = async (onSuccess, onError) => {
+  const response = await fetch(`http://localhost:5000/menu_items/`);
   const result = {
     data: null,
     error: null,
   };
+
+  if (response.ok) {
+    result.data = await response.json();
+    onSuccess();
+  } else {
+    result.error = await response.text();
+    onError();
+  }
   console.log(result.data);
 };
