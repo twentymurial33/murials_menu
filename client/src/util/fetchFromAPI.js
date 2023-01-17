c14f6e70c4c4e0b6840d2f659725c7939d0e1cd2
Murial <34128735+twentymurial33@users.noreply.github.com>
2023-01-17 10:15:48 -0500

    Update client/src/util/fetchFromAPI.js
    Co-authored-by: Brent Clark <brentmclark@gmail.com>

--- client/src/util/fetchFromAPI.js
+++ client/src/util/fetchFromAPI.js
@@ -1,12 +1,5 @@
 export const queryAPI = async (options) => {
   const { url, onSuccess, onError, method } = options;
-  options = {
-    url: "http://localhost:5000/menu_items/",
-    body: null,
-    onSuccess: (result) => console.log(result.data),
-    onError: (result) => console.log(result.error),
-    method: "GET",
-  };
   const result = {
     data: null,
     error: null,
