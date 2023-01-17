60d777cd5c2166ea1ab856c2f5b8d27e8e49769e
Murial Anindo <murial.anindo@lonett.com>
2023-01-16 21:52:46 -0500

    raising an error when no commit message

--- client/src/util/fetchFromAPI.js
+++ client/src/util/fetchFromAPI.js
@@ -2,6 +2,7 @@ export const queryAPI = async (options) => {
   const { url, onSuccess, onError, method } = options;
   options = {
     url: "http://localhost:5000/menu_items/",
+    body: null,
     onSuccess: (result) => console.log(result.data),
     onError: (result) => console.log(result.error),
     method: "GET",
@@ -13,11 +14,11 @@ export const queryAPI = async (options) => {
   const response = fetch(url, method);
   if (response.ok) {
     result.data = await response.json();
-    result.data = response.json();
+    result.data = response.json({ message: "Payload is valid" });
     onSuccess();
   } else {
     result.error = await response.text();
-    result.error = response.text();
+    result.error = response.text({ message: "paylod not correct" });
     onError();
   }
   console.log(result.data);
