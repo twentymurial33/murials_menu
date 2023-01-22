63c0c07e976e1dd407d53efc2e7e48fe86b01f17
Murial Anindo <murial.anindo@lonett.com>
2023-01-21 20:50:07 -0500

    added the tostify error and success

--- client/src/util/fetchFromAPI.js
+++ client/src/util/fetchFromAPI.js
@@ -1,3 +1,5 @@
+import { toast } from "react-toastify";
+
 export const queryAPI = async (options) => {
   const { url, onSuccess, onError, method } = options;
   const result = {
@@ -7,9 +9,13 @@ export const queryAPI = async (options) => {
   const response = fetch(url, method);
   if (response.ok) {
     result.data = await response.json();
+    toast.success();
+    //trigger success toast on success
     onSuccess(result);
   } else {
     result.error = await response.json();
+    toast.error();
+    //Trigger error toast on error
     onError(result);
   }
   console.log(result.data);
--- client/src/util/fetchFromAPI.test.js
+++ client/src/util/fetchFromAPI.test.js
@@ -2,6 +2,6 @@ import { it } from "vitest";
 import { queryAPI } from "./fetchFromAPI";
 
 //Mocking functions
-it("should fetch all items", () => {
+it("test options are properly fecthed", () => {
   queryAPI([]);
 });
