d9fcd3466599cb5c90c277ac8926999f7269dad5
Murial Anindo <murial.anindo@lonett.com>
2023-01-24 15:42:43 -0500

    added 3 unit tests

--- client/src/util/fetchFromAPI.js
+++ client/src/util/fetchFromAPI.js
@@ -1,5 +1,17 @@
 export const queryAPI = async (options) => {
-  const { url, onSuccess, onError, method, toast } = options;
+  if (options === undefined) {
+    throw new Error("options parameter is not defined");
+  }
+  const { url, onSuccess, onError, method } = options;
+  if (url === undefined) {
+    throw new Error("url is not provided");
+  }
+  if (onSuccess === undefined) {
+    throw new Error("onSuccess is not provided");
+  }
+  if (onError === undefined) {
+    throw new Error("onError is not provided");
+  }
   const result = {
     data: null,
     error: null,
@@ -7,9 +19,10 @@ export const queryAPI = async (options) => {
   const response = await fetch(url, method);
   if (response.ok) {
     result.data = await response.json();
+    //make sure onSuccess is there
     onSuccess(result);
   } else {
-    result.error = await response.json();
+    result.error = await response.text();
     onError(result);
   }
   console.log(result.data);
--- client/src/util/fetchFromAPI.test.js
+++ client/src/util/fetchFromAPI.test.js
@@ -1,26 +1,42 @@
 import { expect, test } from "@jest/globals";
 import { queryAPI } from "./fetchFromAPI";
 
-//If the fetch call results in a 200 response, the onSuccess callback is fired
+// fetch call results in a 200 response, the onSuccess callback is fired
 test("should return data with a successful request", async () => {
-  const onSuccess = jest.fn();
-  const onError = jest.fn();
-  return queryAPI()
-    .then(onSuccess)
-    .catch(onError)
-    .finally(() => {
-      expect(onSuccess).toHaveBeenCalled();
-      expect(onError).not.toHaveBeenCalled();
-    });
+  queryAPI().catch((e) => {
+    expect(e.message).toBe("options parameter is not defined");
+  });
+  queryAPI({}).catch((e) => {
+    expect(e.message).toBe("url is not provided");
+  });
+  queryAPI({ url: "/" }).catch((e) => {
+    expect(e.message).toBe("onSuccess is not provided");
+  });
+  queryAPI({ url: "/", onSuccess: jest.fn() }).catch((e) => {
+    expect(e.message).toBe("onError is not provided");
+  });
+});
+//test to confirm that the onError callback is called
+test("confirm onError callback is called", async () => {
+  const options = {
+    url: "http://localhost:5000/menu_items_2023/",
+    onSuccess: jest.fn(),
+    onError: jest.fn(),
+  };
+  await queryAPI(options).then(() => {
+    expect(options.onError).toBeCalled();
+  });
 });
 
-//If the fetch call results in a non-200 response the onError callback is fired
+//test to confirm onSuccess callback
 
-test("should return error as true if api error", async () => {
-  const onSuccess = jest.fn();
-  const onError = jest.fn();
-  expect(onSuccess).toBe(null);
-  expect(onError).toBe(true);
+test("confirm onSuccess callback is called", async () => {
+  const options = {
+    url: "http://localhost:5000/menu_items/",
+    onSuccess: jest.fn(),
+    onError: jest.fn(),
+  };
+  await queryAPI(options).then(() => {
+    expect(options.onSuccess).toBeCalled();
+  });
 });
-
-//If the method is POST a body option cannot be "nullish" (as in, null or undefined) and must be serializable to JSON.
