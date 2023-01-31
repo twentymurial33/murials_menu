6d242a827d7ba2eb3e65c96f2bbb75638a76dadb
Murial Anindo <murial.anindo@lonett.com>
2023-01-31 09:18:32 -0500

    added the reset and update the toEqual

--- client/src/util/fetchFromAPI.test.js
+++ client/src/util/fetchFromAPI.test.js
@@ -1,10 +1,14 @@
 import { expect, test, it } from "@jest/globals";
 import { queryAPI } from "./fetchFromAPI";
+//reset the fetch mock so the previous tests fon't interfere with current tests
+beforeEach(() => {
+  fetch.resetMocks();
+});
 
 it("mocks the fetch request", async () => {
   fetch.mockResponseOnce(JSON.stringify({ foodMenu: { title: "potato" } }));
   const response = await queryAPI();
-  expect(response).toEqual();
+  expect(response).toEqual({ title: "potato" });
   expect(fetch).toHaveBeenCalledTimes(1);
 });
 // fetch call results in a 200 response, the onSuccess callback is fired
