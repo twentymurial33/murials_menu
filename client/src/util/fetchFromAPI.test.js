3d4aa6140052d22e52a426c5689bbe75a08ffd5a
Murial Anindo <murial.anindo@lonett.com>
2023-02-16 17:14:31 -0500

    still working on the test

--- client/src/util/fetchFromAPI.test.js
+++ client/src/util/fetchFromAPI.test.js
@@ -1,4 +1,4 @@
-import { expect, test, it } from "@jest/globals";
+import { expect, it } from "@jest/globals";
 import { queryAPI } from "./fetchFromAPI";
 
 beforeEach(() => {
@@ -16,31 +16,31 @@ it("mocks the fetch request", async () => {
   expect(fetch).toHaveBeenCalledTimes(1);
 });
 
-test("should return data with a successful request", async () => {
-  queryAPI().catch((e) => {
-    expect(e.message).toBe("options parameter is not defined");
-  });
-  queryAPI({}).catch((e) => {
-    expect(e.message).toBe("url is not provided");
-  });
-  queryAPI({ url: "/" }).catch((e) => {
-    expect(e.message).toBe("onSuccess is not provided");
-  });
-  queryAPI({ url: "/", onSuccess: jest.fn() }).catch((e) => {
-    expect(e.message).toBe("onError is not provided");
-  });
-});
+// test("should return data with a successful request", async () => {
+//   queryAPI().catch((e) => {
+//     expect(e.message).toBe("options parameter is not defined");
+//   });
+//   queryAPI({}).catch((e) => {
+//     expect(e.message).toBe("url is not provided");
+//   });
+//   queryAPI({ url: "/" }).catch((e) => {
+//     expect(e.message).toBe("onSuccess is not provided");
+//   });
+//   queryAPI({ url: "/", onSuccess: jest.fn() }).catch((e) => {
+//     expect(e.message).toBe("onError is not provided");
+//   });
+// });
 
-describe("testing the fetch condition", () => {
-  test("should test the conditional statement", () => {
-    const result = {
-      data: null,
-      error: null,
-    };
-    const response = { json: jest.fn().mockResolvedValueOnce(result) };
-    global.fetch = jest.fn().mockResolvedValueOnce(response);
-    return queryAPI().then((data) => {
-      expect(data).toEqual(result);
+describe("test should return a failed response", () => {
+  it("testing the fetch condition", async () => {
+    fetch.mockReject(new Error("error message"));
+    const result = { data: null, error: null };
+    return await queryAPI({
+      url: "/",
+      onSuccess: jest.fn(),
+      onError: jest.fn(),
+    }).then(() => {
+      expect(fetch).toEqual(result.error);
     });
   });
 });
