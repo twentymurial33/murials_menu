e275ce6a3bb2d63f92bbb76dc29970b187cb8855
Murial Anindo <murial.anindo@lonett.com>
2023-02-20 17:18:39 -0500

    removed the counter logic

--- client/src/pages/Details.js
+++ client/src/pages/Details.js
@@ -16,7 +16,6 @@ import "../index.css";
 function Details() {
   const navigate = useNavigate();
   const [search, setNewSearch] = useState("");
-  const [counter, setCounter] = useState(0);
   const [open, setOpen] = useState(false);
   const { isLoading, data, error } = useQuery(["posts"], () =>
     axios("http://localhost:5000/menu_items")
@@ -63,18 +62,6 @@ function Details() {
     handleDelete(id);
   }
 
-  const increaseCounter = () => {
-    setCounter((count) => count + 1);
-  };
-
-  const decreaseCounter = () => {
-    setCounter((count) => count - 1);
-  };
-
-  const reset = () => {
-    setCounter(0);
-  };
-
   return (
     <div>
       <Container>
@@ -89,19 +76,7 @@ function Details() {
             return (
               <div key={data.id}>
                 <img src={data.img} alt="images" />
-                <h4>Food Calories : </h4>
-                {/* <span className="counter__output">{counter}</span> */}
-                {/* <div className="btn__container">
-                  <button className="control__btn" onClick={increaseCounter}>
-                    +
-                  </button>
-                  <button className="control__btn" onClick={decreaseCounter}>
-                    -
-                  </button>
-                  <button className="reset" onClick={reset}>
-                    Reset
-                  </button>
-                </div> */}
+
                 <Box
                   className="boxMenu"
                   lg={{
