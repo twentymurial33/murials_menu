77b2ee9b7a8cdeb3c754f0c5d5250b2104baf047
Murial Anindo <murial.anindo@lonett.com>
2023-06-26 10:17:01 -0400

    added cors on the client level

--- server/index.js
+++ server/index.js
@@ -5,11 +5,14 @@ const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
-app.use(
-  cors({
-    credentials: true,
-  })
-);
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 //endpoint to get Many
