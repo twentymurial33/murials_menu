2ee9f2e79c0545a0ee448126cca8e0e14abefeb7
Murial <34128735+twentymurial33@users.noreply.github.com>
2023-09-19 17:32:32 -0400

    [FEAT]- Logic to add text field in the menucard after adding column in DB (#116)
    * adding logic for text
    
    * adding data.text
    
    * adding data.text
    
    ---------
    
    Co-authored-by: Murial Anindo <murial.anindo@lonett.com>

--- client/src/pages/Details.js
+++ client/src/pages/Details.js
@@ -23,6 +23,7 @@ function Details() {
   const { isLoading, data, error } = useQuery(["posts"], () =>
     axios(`${process.env.REACT_APP_API_URL}/menu_items`)
   );
+  console.log(`${process.env.REACT_APP_API_URL}`);
   if (error) return <h2>Error </h2>;
   if (isLoading) return <h2> isLoading </h2>;
   console.log(data);
@@ -79,12 +80,6 @@ function Details() {
       <Layout />
       <Container>
         <ul className="list">
-          {/* <input
-            className="search"
-            placeholder="Search Food Item"
-            value={search}
-            onChange={handleSetSearch}
-          /> */}
           {filteredMenuItems.map((data) => {
             return (
               <div key={data.id} style={{ display: "flex", margin: "20px" }}>
@@ -105,14 +100,15 @@ function Details() {
                     {data.title}
                   </h1>
                   <p style={{ padding: "30px" }}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    {data.text}
+                    {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                     do eiusmod tempor incididunt ut labore et dolore magna
                     aliqua. Ut enim ad minim veniam, quis nostrud exercitation
                     ullamco laboris nisi ut aliquip ex ea commodo consequat.
                     Duis aute irure dolor in reprehenderit in voluptate velit
                     esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
                     occaecat cupidatat non proident, sunt in culpa qui officia
-                    deserunt mollit anim id est laborum.
+                    deserunt mollit anim id est laborum. */}
                   </p>{" "}
                   <button
                     className="deleteBtn"
